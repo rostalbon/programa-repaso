04ca5c9f89c949febf0aedc898460956dd30b463
Rocío Staltari <rostalbon@gmail.com>
2025-11-06 11:27:05 -0300

    Se modificó la clase Hospital

--- hospital.py
+++ hospital.py
@@ -1,5 +1,8 @@
+from consultorio import Consultorio
+
 class Hospital:
-    def __init__(self, nombre, direccion, cant_consultorios):
+    def __init__(self, nombre, direccion, cant_consultorios, numero):
         self.__nombre = nombre
         self.__direccion = direccion
         self.__cant_consultorios = cant_consultorios
+        self.__consultorio = Consultorio(numero)
